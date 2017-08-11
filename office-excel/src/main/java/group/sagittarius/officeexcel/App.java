6176b9ace7adca3e85e5f81c1e725e2ab0b5b608
Alan@AH <weimingming0311@autohome.com.cn>
2017-08-11 16:29:57 +0800

    initialize projects

---
+++ office-excel/src/main/java/group/sagittarius/officeexcel/App.java
@@ -0,0 +1,13 @@
+package group.sagittarius.officeexcel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
